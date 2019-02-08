c3dbe6c2c3f8b6aa96857e2ee7e026bcbe3918f2
gitaramp <csstrzelec2@gmail.com>
2019-02-08 03:55:32 +0100

    Default avatar for users

--- index.js
+++ index.js
@@ -28,11 +28,15 @@ app.get('/', function(req, res){
 
 
 io.on('connection', function(socket){
-  io.emit('chat message', sendFullMessage('Ktoś dołączył do chatu!'), 'join');
+  
+  socket.on('user join', function(nickName){
+    io.emit('chat message', sendFullMessage(`${nickName} dołączył/a do chatu!`), 'join');
+  });
 
   socket.on('chat message', function(msg){
-    console.log(sendFullMessage(msg));
-    io.emit('chat message', sendFullMessage(msg));
+    let avatar = 'default';
+    console.log(sendFullMessage(msg, true));
+    io.emit('chat message', sendFullMessage(msg), avatar);
   });
 
   socket.on('disconnect', function() {
@@ -45,9 +49,15 @@ http.listen(port, function(){
   console.log('listening on *:' + port);
 });
 
-const sendFullMessage = (msg) => {
+const sendFullMessage = (msg, consoleLog = false, avatar) => {
   let hours = new Date().getHours(),
       minutes = new Date().getMinutes(),
-      seconds = new Date().getSeconds();
-  return `[${hours<10 ? '0' + hours : hours}:${minutes<10 ? '0' + minutes: minutes}:${seconds<10 ? '0' + seconds : seconds}] ${msg}`;
+      seconds = new Date().getSeconds(),
+      date = `[${hours<10 ? '0' + hours : hours}:${minutes<10 ? '0' + minutes: minutes}:${seconds<10 ? '0' + seconds : seconds}]`,
+      fullMsg;
+
+  consoleLog ? fullMsg = `${date} ${msg}` : fullMsg = `${date}<figure class='image is-24x24'>
+                                            <img src='https://www.larvalabs.com/public/images/cryptopunks/punk8178.png' alt=''></figure>${msg}`;
+
+  return fullMsg;
 };
